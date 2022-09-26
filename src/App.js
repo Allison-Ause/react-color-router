8f68437af77128862f63db602b5489140d14ecec
Allison Ause <allison.ause@gmail.com>
2022-09-26 16:36:45 -0700

    styling 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import RGB from './components/RGB/RGB';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <NavBar />
       <Switch>
         <Route path="/rgb/:r/:g/:b" component={RGB} />
-        {/* TODO: ADD 404 NOT FOUND ROUTE path="*" */}
+        <Route path="*" component={NotFound} />
       </Switch>
     </main>
   );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Oops!</h1>
+      <h2>Looks like we misplaced that palette...</h2>
+    </div>
+  );
+}
+
+// animate__hinge;
