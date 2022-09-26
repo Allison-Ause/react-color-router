6c86358b98f9988dbcef49c3a4827af335f412a7
Allison Ause <allison.ause@gmail.com>
2022-09-26 16:53:03 -0700

    styled the 404 page to include an animated gradient background and animated text

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,11 @@
+import 'animate.css';
+import './NotFound.css';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Oops!</h1>
+    <div className="not-found">
+      <h1 className="animate__animated animate__hinge">Oops!</h1>
       <h2>Looks like we misplaced that palette...</h2>
     </div>
   );
 }
-
-// animate__hinge;
