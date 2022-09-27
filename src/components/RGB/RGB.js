78f09f4119c90cabda598147aeccd4ee07a3d78d
Allison Ause <allison.ause@gmail.com>
2022-09-27 14:59:05 -0700

    added emojis explosion!

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -1,14 +1,21 @@
 import { useParams } from 'react-router-dom';
 import './RGB.css';
 import 'animate.css';
+import JSConfetti from 'js-confetti';
 
 export default function RGB() {
+  const jsConfetti = new JSConfetti();
   const { r, g, b } = useParams();
   const currentColor = `rgb(${r},${g},${b})`;
+  jsConfetti.addConfetti({
+    emojis: ['💥', '✨'],
+    emojiSize: 35,
+    confettiNumber: 35,
+  });
 
   return (
     <div key={Math.random()} style={{ backgroundColor: currentColor }} className="color-chip">
-      <h1 className="animate__animated animate__fadeInDown">{`rgb(${r},${g},${b})`}</h1>
+      <h1 className="animate__animated animate__fadeInDownBig">{`rgb(${r},${g},${b})`}</h1>
     </div>
   );
 }
